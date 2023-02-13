634538187850a9a1255e5a9c1eed80795738b3bc
adi levy <adilevy156@gmail.com>
2023-02-13 17:12:00 +0200

    netliiify commiiitt

--- client/src/componnents/MyLists.jsx
+++ client/src/componnents/MyLists.jsx
@@ -47,8 +47,9 @@ const MyLists = () => {
 
     }
     const handleGenerageQr=(val)=>{
-        const url=`https://imaginative-conkies-9422c1.netlify.app/list/${val}`
+        const url=`https://main--imaginative-conkies-9422c1.netlify.app/list/${val}`
         setUrl(url);
+        console.log(url)
         setQrGenerate(true)
     }
     return (
